9591035008db4219283fecfdcfda6a97f39e3f54
Liliya <mechdigits@gmail.com>
2026-01-18 13:03:02 +0200

    Update script-4.js

--- lesson_18/js/script.js
+++ lesson_18/js/script.js
@@ -5,8 +5,8 @@ document.addEventListener('click', documentActions);
 function documentActions(e) {
 	const targetElement = e.target;
 
-	if (targetElement.closest('.burger')) {
+	if (targetElement.closest('.icon-menu')) {
 		document.documentElement.toggleAttribute('data-menu-open');
-		//document.documentElement.classList.toggle('is-lock');
 	}
 }
+
